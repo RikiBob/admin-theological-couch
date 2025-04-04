3d2650b129b4f57b30a09596600bb639f6c56a71
rikibob <bobiar0804@gmail.com>
2025-04-04 17:35:41 +0300

    fix: align content in .content-wrapper

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,8 @@ body, html {
   margin: 0;
   padding: 0;
   height: 100%;
+  display: flex;
+  min-height: 100vh;
   overflow-y: auto;
 }
 
@@ -95,12 +97,13 @@ body, html {
 /* Content wrapper and Info box */
 .content-wrapper {
   display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
   flex-grow: 1;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
   margin-top: 50px;
   padding: 50px;
+  box-sizing: border-box;
 }
 
 .info-box {
@@ -109,7 +112,6 @@ body, html {
   border-radius: 20px;
   padding: 40px;
   width: 100%;
-  min-height: 300px;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.15);
 }
 
